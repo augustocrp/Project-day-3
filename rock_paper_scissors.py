12e921583b35595c9b462353a17318b772bf6804
Augusto Cesar <dev.augusto.crp@gmail.com>
2022-12-19 23:20:47 -0300

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,6 +3,9 @@ import random
 user_points = 0
 computer_points = 0
 
+options = ["r", "t", "P"]
+     
+
 while True:
     user_choice = input ("Escolha R(Pedra)/T(Tesoura)/P(Papel) ou Q para sair.").lower()
 
@@ -11,5 +14,18 @@ while True:
 
     computer_choice = random.randint(0, 2)
     # 0 : R, 1 : T, 2 : P
+    computer_option = options[computer_choice]
+
+    if user_choice == computer_option:
+        print("Empate!")
+    elif user_choice == "r" and computer_option == "t"
+        print("Você ganhou!")
+        user_points = user_points + 1
+    elif user_choice == "p" and computer_option == "r"
+        print("Você ganhou!")
+        user_points = user_points + 1
+    elif user_choice == "r" and computer_option == "t"
+        print("Você ganhou!")
+        user_points = user_points + 1
 
 print ("Goodbye!")
