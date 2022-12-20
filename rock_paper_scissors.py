0dafc2bf76a4ac3c5fde9cc89a5e7497819c36a4
Augusto Cesar <dev.augusto.crp@gmail.com>
2022-12-19 23:14:13 -0300

    Create rock_paper_scissors.py

---
+++ rock_paper_scissors.py
@@ -0,0 +1,15 @@
+import random
+
+user_points = 0
+computer_points = 0
+
+while True:
+    user_choice = input ("Escolha R(Pedra)/T(Tesoura)/P(Papel) ou Q para sair.").lower()
+
+    if user_choice == 'q':
+        break
+
+    computer_choice = random.randint(0, 2)
+    #
+
+print ("Goodbye!")
